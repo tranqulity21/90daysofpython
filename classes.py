8ae6b2ed5b53a833cdbee941292db231d378b0d0
Loise kamau <79315856+tranqulity21@users.noreply.github.com>
2023-06-29 15:28:52 +0300

    access class variable

---
+++ classes.py
@@ -0,0 +1,5 @@
+class Dog:
+  Breed = "SouthAfrican Boerboel";
+  info = "The Boerboel is a large, muscular working breed from South Africa known for its loyalty, protective nature, and confident temperament"
+print(Dog.Breed)
+print(Dog.info)
