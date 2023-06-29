368a4d944b9841288bb220411f82958b86e73027
Loise kamau <79315856+tranqulity21@users.noreply.github.com>
2023-06-29 16:15:05 +0300

    Update instance variable II.py

--- instance variable II.py
+++ instance variable II.py
@@ -4,7 +4,7 @@ class Dog:
     print("Teeshah kennels")
 #instance variable
     #take name passed in and put it in a variable
-    self.name = name 
+    self.namevar = name 
 
 dog1 = Dog("cookie")
 dog2 = Dog("mace")
@@ -12,5 +12,5 @@ dog2 = Dog("mace")
 #instance variables are individual to a particular object
 
 #creating instance class afterobject is created
-print(dog1.name)
-print(dog2.name)
+print(dog1.namevar)
+print(dog2.namevar)
