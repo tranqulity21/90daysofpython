fafa4a8cde19941d29a2e9eb5b8d75a3734485bd
Loise kamau <79315856+tranqulity21@users.noreply.github.com>
2023-06-29 16:00:54 +0300

    Creating variables of instance using two ways.py

---
+++ Instance variables.py
@@ -0,0 +1,24 @@
+#create instance variables through 2 ways
+#1.inside the init function
+#2.after object is created
+import random
+class Dog:
+  Breed = "SouthAfrican Boerboel";
+  info = "The Boerboel is a large, muscular working breed from South Africa known for its loyalty, protective nature, and confident temperament"
+#function runs everytime an object is created from the class
+  def __init__(self):
+    print("Teeshah kennels")
+#instance variable
+    self.puppy_number = (random.randint(1,10))
+  
+print(Dog.Breed)
+print(Dog.info)#access class variable
+
+dog1 = Dog()
+dog2 = Dog()
+
+#instance variables are individual to a particular object
+
+#creating instance class afterobject is created
+dog1.name = "Cookie"
+print(dog1.name)
