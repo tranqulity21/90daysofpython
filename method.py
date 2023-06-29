c88d4a7451b9e4f299fdb804deb1f7dad3922c29
Loise kamau <79315856+tranqulity21@users.noreply.github.com>
2023-06-29 18:02:35 +0300

    method using instance variables

---
+++ method.py
@@ -0,0 +1,17 @@
+#method is a function inside a class
+#showcase a method that uses one of yoyur instance variables
+import random
+class Dog:
+  def __init__(self, name):
+    print("Teeshah kennels")
+    self.name = name
+    self.puppy_age = random.randint(1,10)
+  #method that uses instance variables  
+  def bark(self):
+    print(f"aouf! i'm {self.name} and i'm {self.puppy_age} years")
+    
+dog1 = Dog("cookie")
+dog2 = Dog("mace")
+#call the method
+dog1.bark()
+dog2.bark()
